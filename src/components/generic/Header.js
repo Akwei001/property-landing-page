0ec2d736c423b761a0ecd6952a303bb174626291
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-03-31 10:59:05 +0100

    feature section image added

--- src/components/generic/Header.js
+++ src/components/generic/Header.js
@@ -1,18 +1,16 @@
-import React from "react";
+import React from 'react';
 
 const Header = () => {
-	return (
-		<div className="bg-transparent py-4">
-			<div className="m-auto max-w-4xl px-2 flex items-center justify-between">
-				<h2 className="text-2xl text-midnight-blue font-bold">
-					Lorem
-				</h2>
-				<div className="flex items-center text-midnight-blue font-medium">
-					<p className="text-lg"></p>
-				</div>
-			</div>
-		</div>
-	);
-}
+  return (
+    <div className='py-4 bg-transparent'>
+      <div className='flex items-center justify-between max-w-4xl px-2 m-auto'>
+        <h2 className='text-2xl font-bold text-midnight-blue'>LifeWay</h2>
+        <div className='flex items-center font-medium text-midnight-blue'>
+          <p className='text-lg'></p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
